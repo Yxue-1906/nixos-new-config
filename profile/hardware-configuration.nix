ef212b51c00546a8a077954c468f5222b1f1f0e5
nanashi <na@na.shi>
2025-09-19 00:43:25 +0800

    feat: update flake input, remove some kernel params, update git hook

--- profile/hardware-configuration.nix
+++ profile/hardware-configuration.nix
@@ -38,7 +38,7 @@
   services.xserver.videoDrivers = [ "nvidia" ];
 
   # see: https://download.nvidia.com/XFree86/Linux-x86_64/575.64/README/powermanagement.html#PreserveAllVide719f0
-  boot.kernelParams = [ "nvidia.NVreg_TemporaryFilePath=/nvidia-ram" ];
+  # boot.kernelParams = [ "nvidia.NVreg_TemporaryFilePath=/nvidia-ram" ];
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
