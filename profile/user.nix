3894baa4b81dc24c73ffa39481b8709bd6bae676
nanashi <na@na.shi>
2025-10-26 00:07:49 +0800

    feat: let password immutable

--- profile/user.nix
+++ profile/user.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, secrets, ...}: {
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.mutableUsers = false;
   users.users.unrelated = {
     isNormalUser = true;
     password = secrets.password;
